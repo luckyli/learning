8346eb883f4305c42d38c0694a46afb6426c8ad6
luckyli <48585478@qq.com>
2021-05-10 16:27:50 +0000

    Deploying to main from  @ 535f6468f7f820496bbcf3e89520f9aa4439fe8f 🚀

--- src/common/utils/math.js
+++
@@ -1,8 +0,0 @@
-export function abs (a) {
-  if (typeof a !== 'number') {
-    throw new TypeError('参数必须为数值型')
-  }
-
-  if (a < 0) return -a
-  return a
-}
--- test/test.spec.js
+++
@@ -1,8 +0,0 @@
-import { abs } from '../src/common/utils/math'
-
-test('abs', () => {
-  expect(abs(1)).toBe(1)
-  expect(abs(0)).toBe(0)
-  expect(abs(-1)).toBe(1)
-  expect(() => abs('abc')).toThrow(TypeError) // 类型错误
-})
