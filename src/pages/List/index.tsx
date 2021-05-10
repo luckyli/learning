6639b32b7bb90202c2e5228848aa413eff3c96d1
luckyli <48585478@qq.com>
2021-05-10 23:33:45 +0800

    init

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.scss'
+
+function List () {
+  return (
+    <div className='list page'>
+      <header className='list-header'>
+        <p>Hello List!</p>
+      </header>
+    </div>
+  )
+}
+
+export default List
